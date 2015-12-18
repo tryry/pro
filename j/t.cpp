3e5cc43bbc867cdde97c1477f3a7fc25b95b9225
tryry <1171678767@qq.com>
2015-12-18 23:04:57 +0800

    j/

---
+++ j/t.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+    > File Name: t.cpp
+    > Author: Tolic
+    > Mail: mr_king1994@163.com 
+    > Created Time: Fri 18 Dec 2015 11:04:22 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+
+
+
+    return 0;
+}
